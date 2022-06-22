11f388fa0f44b9fe3932ec28c0b56c779381bec5
Cătălina Grămadă <catalina.g1808@gmail.com>
2022-06-22 13:43:35 +0300

    Small UI changes

--- app/src/main/java/com/example/lostmypet/fragments/WelcomeFragment.java
+++ app/src/main/java/com/example/lostmypet/fragments/WelcomeFragment.java
@@ -56,7 +56,6 @@ public class WelcomeFragment extends Fragment {
         view.findViewById(R.id.btn_register).setOnClickListener(v -> goToRegister());
         view.findViewById(R.id.btn_login).setOnClickListener(v -> goToLogin());
 
-        // change the language with the help of LocaleHelper class
         view.findViewById(R.id.btn_romanian).setOnClickListener(view1 -> setLocale("ro"));
         view.findViewById(R.id.btn_english).setOnClickListener(view12 -> setLocale("en"));
     }
